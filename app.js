919e8af5aa1fe35e24e7c258b86e93328ee2e4c7
munroe1786 <era3munroe@gmail.com>
2022-03-22 07:14:02 -0500

    add 2 of each image to cardArray in app.js

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
 const cardArray = [
     {
         name: 'fries',
-        img:
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
     }
 ]
+
+console.log(cardArray)
